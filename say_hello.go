6e63a6ee86f72e7d65622bc5483f349361560ba0
sugengharianto123 <sugengharianto168@gmail.com>
2026-06-06 16:02:36 +0700

    major upgrade practice

--- say_hello.go
+++ say_hello.go
@@ -1,9 +1,13 @@
 package go_say_hello
 
-func SayHello(name string) string {
-	return "Hello " + name + "!"
+func SayHello() string {
+	return "Hello !"
 }
 
 func SayLoveYou(name string) string {
 	return "I love you " + name + "!"
+}
+
+func SayHappyBirthday(name string, age int) string {
+	return "Happy Birthday " + name + "! You are now " + string(age) + "."
 }
